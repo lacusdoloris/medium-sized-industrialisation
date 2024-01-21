975825b78cc98f84e1999ae8f82adec4caec02d6
Lura Skye <l@veriny.tf>
2024-01-21 06:25:00 +0000

    make the extruder work in LV

---
+++ src/server/material_tiers/extruder.js
@@ -0,0 +1,48 @@
+
+// https://sourcegraph.com/github.com/GregTechCEu/GregTech-Modern@e9a5704a58dd5735b2f445c31f440775760b2312/-/blob/src/main/java/com/gregtechceu/gtceu/data/recipe/generated/PartsRecipeHandler.java
+// gears: material mass * 5
+// small gears: material mass alone
+
+// TODO: Consider keeping the tier multiplier for above-2800K items only.
+
+/**
+ * Moves down all relevant extruder recipes to LV.
+ * 
+ * @param {Internal.RecipesEventJS} event
+ */
+export const fixExtruderRecipeTier = (event) => {
+    GTRegistries.MATERIALS.forEach((material) => {
+        if (material.hasFlag(GTMaterialFlags.GENERATE_GEAR)) {
+            event.remove({id: `gtceu:extruder/extrude_${material.name}_ingot_to_gear`});
+
+            event.recipes.gtceu.extruder(`nijika:auto/gears/regular/${material.name}`)
+                .itemInputs(`4x #forge:ingots/${material.name}`)
+                .notConsumable(GTItems.SHAPE_EXTRUDER_GEAR.get())
+                .itemOutputs(`gtceu:${material.name}_gear`)
+                .EUt(32)
+                .duration(material.mass * 5);
+        }
+
+        if (material.hasFlag(GTMaterialFlags.GENERATE_SMALL_GEAR)) {
+            event.remove({id: `gtceu:extruder/extrude_${material.name}_ingot_to_small_gear`});
+            
+            event.recipes.gtceu.extruder(`nijika:auto/gears/small/${material.name}`)
+                .itemInputs(`#forge:ingots/${material.name}`)
+                .notConsumable(GTItems.SHAPE_EXTRUDER_GEAR_SMALL.get())
+                .itemOutputs(`gtceu:${material.name}_small_gear`)
+                .EUt(32)
+                .duration(material.mass);
+        }
+
+        if (material.hasFlag(GTMaterialFlags.GENERATE_RING)) {
+            event.remove({id: `gtceu:extruder/extrude_${material.name}_ingot_to_ring`});
+
+            event.recipes.gtceu.extruder(`nijika:auto/rings/${material.name}`)
+                .itemInputs(`#forge:ingots/${material.name}`)
+                .notConsumable(GTItems.SHAPE_EXTRUDER_RING.get())
+                .itemOutputs(`gtceu:${material.name}_ring`)
+                .EUt(32)
+                .duration(material.mass);
+        }
+    });
+}
--- src/server/material_tiers/index.js
+++ src/server/material_tiers/index.js
@@ -1,7 +1,9 @@
 import { rewriteComponentTieredRecipes } from "./components";
 import { addCreateRecipes } from "./create"
+import { fixExtruderRecipeTier } from "./extruder";
 
 export const adjustMaterialTierRecipes = (event) => {
     addCreateRecipes(event);
     rewriteComponentTieredRecipes(event);
+    fixExtruderRecipeTier(event);
 }
