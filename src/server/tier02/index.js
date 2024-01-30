f3a68d969a539586e7a17c84438df0c8bf728453
Lura Skye <l@veriny.tf>
2024-01-30 14:52:42 +0000

    add evaporation tower recipe

--- src/server/tier02/index.js
+++ src/server/tier02/index.js
@@ -1,7 +1,25 @@
 
 // MV!
+
+import { GT_MACHINE_TIERS } from "../../shared/definition";
+
 /** @param {Internal.RecipesEventJS} event */
 export const doTier02Content = (event) => {
+    let mvTier = GT_MACHINE_TIERS[1];
+
+    event.shaped(
+        "gtceu:brine_tower",
+        ["RGR", "CHC", "PBP"],
+        {
+            R: mvTier.materials.rotor.tagged("rotors"),
+            G: mvTier.materials.glass,
+            C: "#gtceu:circuits/mv",
+            H: "gtceu:mv_machine_hull",
+            P: "gtceu:mv_electric_pump",
+            B: "#forge:plates/bronze"
+        }
+    ).id("nijika:tier02/brine_evaporation");
+
     event.recipes.gtceu.brine_tower("nijika:tier02/basic_brine_production")
         .inputFluids(Fluid.of("minecraft:water").withAmount(250 * FluidAmounts.BUCKET))
         .outputFluids(Fluid.of("gtceu:salt_water").withAmount(50 * FluidAmounts.BUCKET))
