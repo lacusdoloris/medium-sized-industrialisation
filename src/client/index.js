d143df0633c482e8660dfe7df6f6c9b3cf5e73cd
Lura Skye <l@veriny.tf>
2024-02-19 05:21:06 +0000

    add tooltip to umbrella and give it in the starting quest.

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+/**
+ * Adds tooltips to various items.
+ */
+ItemEvents.tooltip((event) => {
+    event.add("wenospeakumbrellarino:umbrella", Component.translatable("nijika.tooltip.umbrella"));
+})
