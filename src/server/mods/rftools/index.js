bc0717dce1f475b1d53805dff8a4542a8b90bdbe
Lura Skye <fuyukaii@proton.me>
2024-02-25 19:47:53 +0000

    rework a couple of rftools recipes
    no longer needs end stone.

--- src/server/mods/rftools/index.js
+++ src/server/mods/rftools/index.js
@@ -58,4 +58,14 @@ export const adjustRfToolsRecipes = (event) => {
             C: "rftoolscontrol:card_base",
         })
         .id("nijika:mods/rftools/arm4tdmi");
+
+    event.remove({ id: "rftoolsbase:machine_base" });
+    event.shaped(
+        "rftoolsbase:machine_base",
+        ["III", "SSS"],
+        {
+            I: "#forge:nuggets/red_alloy",
+            S: "minecraft:stone_slab"  // todo: stone slabs tag?
+        }
+    ).id("nijika:mods/rftools/machine_base");
 };
