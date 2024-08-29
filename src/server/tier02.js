19ce48fc6e60e6708db9b72b065e053e29d92eeb
Lura Skye <fuyukaii@proton.me>
2024-08-29 13:58:09 +0100

    add recipe for bulk ore washer

--- src/server/tier02.js
+++ src/server/tier02.js
@@ -79,4 +79,13 @@ export const doTier02Content = (event) => {
         .EUt(GTValues.VA[GTValues.MV])
         .duration(10 * 20)
         .circuit(2);
+
+    // *does* require MV circuits, unlike the ball bearing mill.
+    event.shaped("gtceu:washing_channel", ["GGG", "FCF", "SMS"], {
+        G: mvTier.materials.glass,
+        F: "gtceu:item_filter",
+        C: mvTier.circuitTag,
+        S: "#forge:plates/steel",
+        M: "gtceu:mv_electric_motor"
+    }).id("nijika:tier02/ore_washing_channel");
 };
