1e6b9715cc6902b3a29f60fcab40c8e7c07f04b2
Lura Skye <fuyukaii@proton.me>
2024-09-02 11:59:51 +0100

    revamp ore sorting ores
    - all bocchi ores now produce one of the orestone ores
    - they produce one EV+ ore
    - they produce gemstones or salts at tier 2 sorting

--- src/shared/ores/bocchi.js
+++ src/shared/ores/bocchi.js
@@ -4,49 +4,56 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { getMaterial, getStackForTagPrefix, nijikaId } from "../utils";
+import { getStackForTagPrefix, nijikaId } from "../utils";
 
 // todo: insert vanadite
+//
+// The orestones are fine, but they scale poorly due to a lack of overclocks and requiring you to
+// find the actual vents.
+//
+// Ore drills can be stacked much more densely in an ore chunk, and can produce multiple ores via
+// ore sorting. Later on, the Rock Synthesis Plant Kai will be able to outdo both. These are
+// designed to *complement* the orestones, not entirely replace them.
 
 export const BASE_ORES = {
     bocchinium: {
         colour: 0xf7a0b5,
         iconSet: GTMaterialIconSet.BRIGHT,
         seed: 809651466,
-        intoOres: ["ilmenite", "monazite", "realgar"],
-        gem: "amethyst",
+        intoOres: ["hematite", "ilmenite", "diamond"],
+        runoff: "hydrochloric",
     },
 
     nijikaite: {
         seed: 1929496478,
         colour: 0xf3e5a1,
         iconSet: GTMaterialIconSet.BRIGHT,
-        intoOres: ["beryllium", "scheelite", "fluorite"],
-        gem: "emerald",
+        intoOres: ["tetrahedrite", "molybdenite", "emerald"],
+        runoff: "sulfuric",
     },
 
     ryoite: {
         seed: 155659298,
         colour: 0x49679f,
         iconSet: GTMaterialIconSet.BRIGHT,
-        intoOres: ["pyrolusite", "gold", "hematite"],
-        gem: "sapphire",
+        intoOres: ["cassiterite", "scheelite", "topaz"],
+        runoff: "fluoric",
     },
 
     kitakitaite: {
         seed: 649130079,
         colour: 0xd2625a,
         iconSet: GTMaterialIconSet.BRIGHT,
-        intoOres: ["pentlandite", "tantalite", "asbestos"],
-        gem: "ruby",
+        intoOres: ["silver", "gold", "tricalcium_phosphate"],
+        runoff: "nitric",
     },
 
     kikurite: {
         seed: 1321746503,
         colour: 0x995678,
         iconSet: GTMaterialIconSet.DULL,
-        intoOres: ["molybdenite", "tricalcium_phosphate", "stibnite"],
-        gem: "opal",
+        intoOres: ["bauxite", "tantalite", "salt"],
+        runoff: "hydrochloric",
     },
 
     /*yoyokite: {
@@ -100,22 +107,27 @@ export const addBaseOreRecipes = (event) => {
             .fluid(Fluid.of("gtceu:drilling_fluid").withAmount(165 * FluidAmounts.MILLIBUCKET))
             .id(`nijika:drilling/overworld_vanilla/${name}`);
 
-        // Similar to Angel's Refining.
+        // Similar to Angel's Refining, but with only one extra ore per tier.
         //
-        // Crushed ore sorting: ore 1 + 2 + slag
-        // Impure ore (chunks): ore 1 + 2 + 3 + slag
-        // Refined ore (crystals): ore 1 + 2 + 3 + 4 + slag
-        // Pure ore (... yeah): all 7 ores, no slag
+        // Crushed ore sorting: ore 1 + 2 + runoff
+        // Impure ore (chunks): ore 1 + 2 + 3 + runoff
+        // Refined ore (crystals): wip
+        // Pure ore (... yeah): wip
 
         event.remove({ input: `gtceu:crushed_${name}_ore` });
         event.recipes.gtceu
             .ore_sorting(`nijika:base_ores/${name}/sorting_tier_1`)
-            .itemInputs(`32x gtceu:crushed_${name}_ore`)
-            .itemOutputs(
-                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[0]).withCount(16),
-                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[1]).withCount(8)
+            .itemInputs(
+                `64x gtceu:crushed_${name}_ore`,
+                `64x gtceu:crushed_${name}_ore`,
+                `64x gtceu:crushed_${name}_ore`
             )
-            .duration(5 * 20)
+            .itemOutputs(
+                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[0]).withCount(64),
+                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[0]).withCount(32),
+                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[1]).withCount(64)
+            )
+            .duration(50 * 20)
             .EUt(GTValues.VA[GTValues.HV])
             .circuit(1);
 
@@ -127,24 +139,28 @@ export const addBaseOreRecipes = (event) => {
             .itemInputs(`64x gtceu:crushed_${name}_ore`)
             .inputFluids(Fluid.of("gtceu:distilled_water").withAmount(9600 * FluidAmounts.MB))
             .itemOutputs(`64x gtceu:impure_${name}_dust`)
-            .itemOutputsRanged(
-                getStackForTagPrefix(TagPrefix.rawOre, getMaterial(oreData.gem)),
-                15,
-                35
-            )
             .EUt(GTValues.VHA[GTValues.MV])
             .duration(20 * 20);
 
-        event.recipes.gtceu
-            .ore_sorting(`nijika:base_ores/${name}/sorting_tier_2`)
-            .itemInputs(`32x gtceu:impure_${name}_dust`)
-            .itemOutputs(
-                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[0]).withCount(16),
-                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[1]).withCount(16),
-                getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[2]).withCount(8)
-            )
-            .duration(5 * 20)
-            .EUt(GTValues.VA[GTValues.EV])
-            .circuit(2);
+        // tier 2 sorting
+        if (oreData.intoOres.length > 2) {
+            event.recipes.gtceu
+                .ore_sorting(`nijika:base_ores/${name}/sorting_tier_2`)
+                .itemInputs(
+                    `64x gtceu:crushed_${name}_ore`,
+                    `64x gtceu:crushed_${name}_ore`,
+                    `64x gtceu:crushed_${name}_ore`
+                )
+                .itemOutputs(
+                    getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[0]).withCount(64),
+                    getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[0]).withCount(64),
+                    getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[1]).withCount(64),
+                    getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[1]).withCount(32),
+                    getStackForTagPrefix(TagPrefix.rawOre, oreData.intoOres[2]).withCount(32)
+                )
+                .duration(75 * 20)
+                .EUt(GTValues.VA[GTValues.EV])
+                .circuit(1);
+        }
     }
 };
