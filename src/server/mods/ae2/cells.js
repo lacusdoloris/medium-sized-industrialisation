4b00178e76f5f3873710c13d6b1575139994ab85
Lura Skye <fuyukaii@proton.me>
2024-09-18 23:32:02 +0100

    make fluid cells use creosote instead of ptfe

--- src/server/mods/ae2/cells.js
+++ src/server/mods/ae2/cells.js
@@ -4,9 +4,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+
+/**
+ * Adjusts the recipes for AE2 storage cells.
+ * 
+ * @param {Internal.RecipesEventJS} event
+ */
 export const adjustAe2StorageCellRecipes = (event) => {
-    // remove regular shaped asssembly recipes for storage cells
-    // the end-of-line marker for this means it only matches the recipes
+    // remove regular shaped assembly recipes for storage cells
     event.remove({
         type: "minecraft:crafting_shaped",
         id: /^ae2:network\/cells\/(?:item|fluid)_storage_cell_[0-9]+k$/,
@@ -31,22 +36,13 @@ export const adjustAe2StorageCellRecipes = (event) => {
         .EUt(GTValues.VA[GTValues.LV])
         .duration(5 * 20);
 
-    // require PTFE for fluid cells.
-    event
-        .shaped("ae2:fluid_cell_housing", ["FRF", "R R", "PPP"], {
-            F: "ae2:quartz_glass",
-            R: "#forge:dusts/redstone",
-            P: "#forge:plates/polytetrafluoroethylene",
-        })
-        .id("ae2:network/cells/fluid_cell_housing");
-
+    // Similar to how fluid tanks are made, these are just coated in creosote.
+    event.remove({id: "ae2:network/cells/fluid_cell_housing"});
     event.recipes.gtceu
-        .assembler("nijika:mods/ae2/fluid_cell_housing_assembler")
-        .itemInputs("2x ae2:quartz_glass", "3x #forge:dusts/redstone")
-        .inputFluids(
-            Fluid.of("gtceu:polytetrafluoroethylene").withAmount(144 * 3 * FluidAmounts.MB)
-        )
-        .itemOutputs("ae2:fluid_cell_housing")
+        .assembler("nijika:mods/ae2/fluid_cell_housing")
+        .itemInputs("1x ae2:item_cell_housing")
+        .inputFluids(Fluid.of("gtceu:creosote").withAmount(2 * FluidAmounts.BUCKET))
+        .itemOutputs("1x ae2:fluid_cell_housing")
         .EUt(GTValues.VA[GTValues.LV])
         .duration(5 * 20);
 };
