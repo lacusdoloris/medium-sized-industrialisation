caf44b59d5898dec286db100c25f813882546df5
Loris Lacuna <doriloris@protonmail.com>
2025-04-10 02:28:28 +0100

    remove most overworld structures

--- src/startup/worldgen.js
+++ src/startup/worldgen.js
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// why is this a startup event? it's a mystery/
+// why is this a startup event? it's a mystery
 
 /** @param {Internal.RemoveWorldgenEventJS} event */
 export const adjustWorldgenRemovals = (event) => {
